f0786fb660dcd4e76c70ff4fc4a59540a4a747ed
Shahriar Ahmed <shahriarabiddut@gmail.com>
2025-01-23 20:34:01 +0600

    State not found error

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,12 @@
 const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com']
+  },env: {
+    KINDE_SITE_URL: process.env.KINDE_SITE_URL ?? `https://${process.env.VERCEL_URL}`,
+    KINDE_POST_LOGOUT_REDIRECT_URL:
+      process.env.KINDE_POST_LOGOUT_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}`,
+    KINDE_POST_LOGIN_REDIRECT_URL:
+      process.env.KINDE_POST_LOGIN_REDIRECT_URL ?? `https://${process.env.VERCEL_URL}/profile`
   }
 };
 
